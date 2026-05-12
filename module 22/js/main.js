73f59d5645913759a91c553a43113e165922e447
endritdemiri <endritdemiri202@gmail.com>
2026-05-12 20:47:39 +0200

    java

---
+++ module 22/js/main.js
@@ -0,0 +1,24 @@
+console.log("Goodbye")
+
+var x = 10;
+
+var _value = "digital school"
+
+//1.   Numbers: 5, 6.5, 7, etc.
+
+//2.    String: “Hello GeeksforGeeks” etc.
+
+//3.    Boolean: Represent a logical entity and can have two values: true or false.
+
+//4.    Null: This type has only one value: null.
+
+//5.    Undefined: A variable that has not been assigned a value is undefined.
+
+//6.    Object: It is the most important data type and forms the building blocks for modern JavaScript.
+
+
+//The name must start with a letter (a to z or A to Z), underscore( _ ), or dollar( $ ) sign.
+
+// After the first letter we can use digits (0 to 9), for example, value1.
+
+//  JavaScript variables are case sensitive, for example, x and X are different variables

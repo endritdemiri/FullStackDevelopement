812e4953053a83240e6a4ab96d9fc5a20825d579
endritdemiri <endritdemiri202@gmail.com>
2026-05-28 20:47:43 +0200

    challange

---
+++ challange/main.js
@@ -0,0 +1,22 @@
+var square = document.getElementById('square')
+var circle = document.getElementById('circle')
+var triangle = document.getElementById('triangle')
+var rectangle = document.getElementById('rectangle')
+
+square.onclick = function(){
+    square.setAttribute('class','square')
+
+}
+
+circle.onclick = function(){
+    circle.setAttribute('class','circle')
+}
+
+triangle.onclick = function(){
+    triangle.setAttribute('class','triangle')
+}
+
+rectangle.onclick = function(){
+  rectangle.setAttribute('class','rect')
+}
+
